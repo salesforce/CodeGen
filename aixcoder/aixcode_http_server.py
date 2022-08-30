03eca5a4a154ebe2ec3c3dcb8bc10f5149501472
chenguangjian.jk <chenguangjian.jk@bytedance.com>
2022-08-31 02:25:25 +0800

    @run_on_executor

--- aixcoder/aixcode_http_server.py
+++ aixcoder/aixcode_http_server.py
@@ -7,6 +7,8 @@
 import tornado.ioloop
 import tornado.web
 import tornado.gen
+from concurrent.futures import ThreadPoolExecutor
+from tornado.concurrent import run_on_executor
 import json
 
 from aixcoder.aixcode import AIXCode
@@ -22,7 +24,6 @@ def get_body_json(body):
 
 
 class PingHandler(tornado.web.RequestHandler):
-
     @tornado.gen.coroutine
     def get(self):
         print(f'request:{self.request.full_url()}')
@@ -30,46 +31,62 @@ class PingHandler(tornado.web.RequestHandler):
 
     @tornado.gen.coroutine
     def post(self):
+        print(f'request:{self.request.full_url()}')
         body_json = get_body_json(self.request.body)
         print(f'request:{body_json}')
         self.write("Pong!")
 
 
 class AIX1Handler(tornado.web.RequestHandler):
+    executor = ThreadPoolExecutor(32)
+
+    @run_on_executor
+    def aixcode(self, x):
+        return AIXCode1.aixcode(x)
 
     @tornado.gen.coroutine
     def get(self):
         """get请求"""
         print(f'request:{self.request.full_url()}')
         x = self.get_argument('x')
-        self.write(AIXCode1.aixcode(x))
+        y = yield self.aixcode(x)
+        self.write(y)
 
     @tornado.gen.coroutine
     def post(self):
         '''post请求'''
+        print(f'request:{self.request.full_url()}')
         body_json = get_body_json(self.request.body)
         print(f'request:{body_json}')
-
         x = body_json.get("x")
-        self.write(AIXCode1.aixcode(x))
+        y = yield self.aixcode(x)
+        self.write(y)
 
 
 class AIX2Handler(tornado.web.RequestHandler):
+    executor = ThreadPoolExecutor(32)
+
+    @run_on_executor
+    def aixcode(self, x):
+        return AIXCode2.aixcode(x)
 
     @tornado.gen.coroutine
     def get(self):
         """get请求"""
         print(f'request:{self.request.full_url()}')
         x = self.get_argument('x')
-        self.write(AIXCode2.aixcode(x))
+        y = yield self.aixcode(x)
+        self.write(y)
 
     @tornado.gen.coroutine
     def post(self):
         '''post请求'''
+        print(f'request:{self.request.full_url()}')
         body_json = get_body_json(self.request.body)
         print(f'request:{body_json}')
         x = body_json.get("x")
-        self.write(AIXCode2.aixcode(x))
+        y = yield self.aixcode(x)
+        self.write(y)
 
 
 if __name__ == "__main__":
